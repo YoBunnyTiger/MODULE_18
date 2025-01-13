3f6d9760df24a0177b9f210e15598dfc70096095
nksound <NKSound@mail.ru>
2025-01-13 19:45:48 +0300

    "Urls и Views. Функциональное и классовое представление."

---
+++ task2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+def func_view(request):
+    return render(request, 'func_template.html')
+
+
+class ClassView(TemplateView):
+    template_name = 'class_template.html'
